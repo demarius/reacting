5742de37ddf9215cfbd62c8004a634eb7155f712
Demarius Chrite <chrite.demarius@gmail.com>
2016-02-10 12:29:33 -0400

    Respond to index request.

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ var browserify = require('browserify'), express = require('express')
 jsx.install()
 
 var index = require('./views/index.jsx')
+
+express.use('/', function (req, res) {
+    res.setHeader('Content-Type', 'text/html')
+    res.end(React.renderToStaticMarkup(
+        React.DOM.body(
+            null,
+            React.DOM.div({
+                id: 'reacting'
+            }),
+            React.DOM.script({
+                'id': 'data',
+                'type':  'text/plain',
+                'data-json': JSON.stringify({})
+            }),
+            React.DOM.script({
+                src: '/bundle.js'
+            })
+        )
+    ))
+})
